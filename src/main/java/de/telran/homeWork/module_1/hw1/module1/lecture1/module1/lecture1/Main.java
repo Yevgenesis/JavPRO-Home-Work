09c828c891a08c78c36253155148f4208ed9544f
yevhenii.ashurov <yevhenii.ashurov@occe>
2023-11-08 21:16:04 +0100

    hw-1

--- src/main/java/de/telran/homeWork/module_1/hw1/module1/lecture1/module1/lecture1/Main.java
+++ src/main/java/de/telran/homeWork/module_1/hw1/module1/lecture1/module1/lecture1/Main.java
@@ -3,13 +3,12 @@ package de.telran.homeWork.module_1.hw1.module1.lecture1.module1.lecture1;
 
 public class Main {
     public static void main(String[] args) {
-/*1.Класс module1.lecture1.module1.lecture1.Person Создать класс
-module1.lecture1.module1.lecture1.Person, который содержит переменные:fullName, age; методы move() и talk(),
-в которых просто вывести на консоль сообщение -"Такой-то  module1.lecture1.module1.lecture1.Person говорит".
-Добавьте два конструктора - module1.lecture1.module1.lecture1.Person() и
-module1.lecture1.module1.lecture1.Person(fullName, age). Создайте два объекта этого класса.
-Один объект инициализируется конструктором module1.lecture1.module1.lecture1.Person(),
-другой - module1.lecture1.module1.lecture1.Person(fullName, age)*/
+/*1. Создать класс module1.lecture1.module1.lecture1.Person, который содержит переменные:
+fullName, age; методы move() и talk(), в которых просто вывести на консоль сообщение -
+"Такой-то  module1.lecture1.module1.lecture1.Person говорит". Добавьте два конструктора -
+module1.lecture1.module1.lecture1.Person() и module1.lecture1.module1.lecture1.Person(fullName, age).
+Создайте два объекта этого класса. Один объект инициализируется конструктором
+module1.lecture1.module1.lecture1.Person(), другой - module1.lecture1.module1.lecture1.Person(fullName, age)*/
 
         Person user1 = new Person();
         user1.fullName = "Valera";
@@ -21,9 +20,8 @@ module1.lecture1.module1.lecture1.Person(fullName, age). Создайте два
         user2.move();
         user2.talk();
 
-/*2. Создайте класс Phone, который содержит переменные number, model и weight.
-Создайте три экземпляра этого класса.
-Выведите на консоль значения их переменных.
+/*2. Создать класс Phone, который содержит переменные number, model и weight.
+Создайте три экземпляра этого класса. Выведите на консоль значения их переменных.
 Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего.
 Выводит на консоль сообщение “Звонит {name}”. Метод getNumber – возвращает номер телефона.
 Вызвать эти методы для каждого из объектов.*/
